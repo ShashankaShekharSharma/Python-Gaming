0c2c7727b2045e12d283d0946801b591580e8abd
Chaitanya Negi <chaitanyanegi1@gmail.com>
2024-01-13 21:27:24 +0530

    added country list

--- Atlas.py
+++ Atlas.py
@@ -1,10 +1,26 @@
 import random
 flag = 1
-a = ['australia', 'argentina', 'america']
-b = ['brazil', 'bangladesh']
-c = ['canada']
-countries = a+b+c
-print(countries)
+country_list=[
+    'Canada', 'US', 'Mexico', 'Guatemala', 'Belize', 'El Salvador', 'Honduras', 'Costa Rica', 'Nicaragua', 'Panama',
+    'Bermuda', 'Bahamas', 'Haiti', 'Cuba', 'Jamaica', 'Dominican Republic', 'Dominica', 'St. Kitts and Nevis',
+    'St. Lucia', 'St. Vincent and The Grenadines', 'Barbados', 'Grenada', 'Trinidad and Tobago', 'Columbia', 'Venezuela',
+    'Guyana', 'Suriname', 'Brazil', 'Ecuador', 'Peru', 'Bolivia', 'Paraguay', 'Chile', 'Argentina', 'Uruguay', 'Kosova',
+    'Palestine', 'Western Sahara', 'Turkish Cyprus', 'Taiwan', 'Vatican City', 'Australia', 'New Zealand', 'Papua New Guinea',
+    'Palau', 'Micronesia', 'Marshall Islands', 'Nauru', 'Vanuatu', 'Solomon Islands', 'Kiribati', 'Tuvalu', 'Tonga', 'Fiji',
+    'Samoa', 'Yemen', 'Oman', 'UAE', 'Qatar', 'Bahrain', 'Saudi Arabia', 'Kuwait', 'Iraq', 'Jordan', 'Israel', 'Lebanon',
+    'Syria', 'Cyprus', 'Turkey', 'Georgia', 'Armenia', 'Azerbaijan', 'Iran', 'Afghanistan', 'Pakistan', 'Turkmenistan',
+    'Uzbekistan', 'Tajikistan', 'Kyrgyzstan', 'Kazakhstan', 'China', 'Mongolia', 'India', 'Nepal', 'Srilanka', 'Maldives',
+    'Bhutan', 'Bangladesh', 'Myanmar', 'Thailand', 'Laos', 'Cambodia', 'Vietnam', 'Malaysia', 'Singapore', 'Brunei', 'Indonesia',
+    'East Timor', 'Philippines', 'South Korea', 'North Korea', 'Japan', 'Morocco', 'Algeria', 'Tunisia', 'Libya', 'Egypt',
+    'Mauritania', 'Mali', 'Senegal', 'Gambia', 'Guinea Bissau', 'Guinea', 'Sierra Leone', 'Liberia', 'Ivory Coast', 'Ghana',
+    'Burkina', 'Togo', 'Benin', 'Niger', 'Nigeria', 'Chad', 'Cameroon', 'Central African Republic', 'North Sudan', 'South Sudan',
+    'Eritrea', 'Djibouti', 'Ethiopia', 'Somalia', 'Equatorial Guinea', 'Gabon', 'Republic of Congo', 'Democratic Republic of Congo',
+    'Uganda', 'Kenya', 'Rwanda', 'Burundi', 'Tanzania', 'Angola', 'Zambia', 'Malawi', 'Mozambique', 'Namibia', 'Botswana',
+    'Zimbabwe', 'South Africa', 'Lesotho', 'Swaziland', 'Seychelles', 'Comoros', 'Madagascar', 'Mauritius', 'Cape Verde',
+    'Sai Tome and Principe'
+]
+
+print(country_list)
 countries_used = []
 def filter_countries_starting_with(letter, countries):
     filtered_countries = [country for country in countries if country.lower().startswith(letter.lower())]
@@ -17,11 +33,11 @@ def filter_countries_starting_with(letter, countries):
       flag = -1
 while flag == 1:
   user_input = input("Player: ").lower()
-  while user_input in countries:
+  while user_input in country_list:
     countries_used.append(user_input)
-    countries.remove(user_input)
-    computer = filter_countries_starting_with(user_input, countries)
-    print("Computer: " + computer.capitalize())
+    country_list.remove(user_input)
+    computer = filter_countries_starting_with(user_input, country_list)
+    print("Computer: " + computer)
   if user_input in countries_used:
     print("Game Over! You Lose")
 
